81d60f80437957775886846947001f11e83c91db
Lenovo-User <Lenovo-User@DESKTOP-TIU29D7>
2024-01-22 13:32:28 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Buiding_a_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // convert a string to a number
+            int num = Convert.ToInt32("45"); // yo int 16 hold a norrower range
+            Console.WriteLine(num+6);
+
+            int num1= Convert.ToInt32(Console.ReadLine());
+            int num2= Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine(num1 + num2);
+            // can also convert to double
+            double num3= Convert.ToDouble(Console.ReadLine());
+            double num4= Convert.ToDouble(Console.ReadLine());  
+            Console.WriteLine(num3 + num4);
+            Console.Read();
+
+        }
+    }
+}
